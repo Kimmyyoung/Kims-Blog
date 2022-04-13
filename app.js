5d19effacc7a36688bc950b9fdc947b5412d3ef2
Kimmy <hiyoung.kimmy@gmail.com>
2022-04-13 10:56:58 -0700

    Add gitignore , Procfile and updates ports

--- app.js
+++ app.js
@@ -116,6 +116,11 @@ app.get('/posts/:postId', function(req, res){
 //express page routing (tip! req.params.postName : day1 )
 
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
